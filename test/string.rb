80e2e5237aeb361829f0cc3ba3dd18e03b6c806f
tufusa <me@tufusa.net>
2026-01-11 21:40:33 +0900

    feat: `String` を定義 (#4)
    * feat: `String` を定義
    
    * feat: `String` のテストを追加
    
    * feat: `String` に比較演算子を定義

---
+++ test/string.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/Void
+# rubocop:disable Lint/RedundantTypeConversion
+# rubocop:disable Style/StringConcatenation
+
+String.new
+String.new 'abcdef'
+
+'abcdef' == true
+
+'abcdef' != false
+
+'abc' < 'def'
+
+'abc' <= 'def'
+
+'abc' > 'def'
+
+'abc' >= 'def'
+
+'abc' + 'bef'
+
+'abcdef' * 100
+
+'abcdef'.size
+
+'abcdef'.length
+
+'123456'.to_i
+'C0FFEE'.to_i 16
+
+'123.456'.to_f
+
+'abcdef'.to_s
+String.to_s
+
+'abc' << 'def'
+
+'abcdef'.slice 2
+'abcdef'.slice 2, 3
+
+'abcdef'[3]
+'abcdef'[3, 3]
+
+'abcdef'[3] = 'D'
+'abcdef'[3, 3] = 'defghi'
+
+"abcdef\n".chomp
+
+"abcdef\n".chomp!
+
+'abcdef'.clear
+
+'abcdef'.dup
+
+''.empty?
+
+'abcdef'.getbyte 3
+
+'abcdef'.setbyte 3, 68
+
+'abcdef'.index 'cd'
+'abcdef'.index 'cd', 3
+
+'abcdef'.inspect
+String.inspect
+
+'abcdef'.ord
+
+'abcdef'.slice! 2
+'abcdef'.slice! 2, 3
+
+'  abcdef'.lstrip
+
+'  abcdef'.lstrip!
+
+'abcdef  '.rstrip
+
+'abcdef  '.rstrip!
+
+' abcdef '.strip
+
+' abcdef '.strip!
+
+'abcdef'.tr 'a-z', 'n-za-m'
+
+'abcdef'.tr! 'a-z', 'n-za-m'
+
+'abcdef'.start_with? 'abc'
+
+'abcdef'.end_with? 'def'
+
+'abcdef'.include? 'cd'
+
+'abcdef'.upcase
+
+'abcdef'.upcase!
+
+'ABCDEF'.downcase
+
+'ABCDEF'.downcase!
+
+'abcdef'.each_byte { |byte| byte }
+
+'abcdef'.each_char { |char| char }
+
+'abcdef'.ljust 10
+'abcdef'.ljust 10, '_'
+
+'abcdef'.rjust 10
+'abcdef'.rjust 10, '_'
+
+"abc\ndef".each_line { |line| line }
+
+# rubocop:enable all
