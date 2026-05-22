f2679bab5ba73db0643013659d96ea19a8b66b7f
tufusa <me@tufusa.net>
2026-05-22 23:25:12 +0900

    feat: `Range` を定義 (#42)
    * feat: `Range` を定義
    
    * chore: テストを追加
    
    * feat: `String#slice`, `String#[]=` に `Range` 引数のオーバーロードを追加

---
+++ test/range.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/Void
+# rubocop:disable Style/CaseEquality
+# rubocop:disable Lint/BinaryOperatorWithIdenticalOperands
+
+0..10
+
+(0..10) === (0..10)
+
+(0..10).first
+
+(0..10).last
+
+(0...10).exclude_end?
+
+(0..10).inspect
+Range.inspect
+
+(0..10).to_s
+Range.to_s
+
+# rubocop:enable all
