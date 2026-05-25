22cfc7853b9f8cb10d4512f2fcc5c3332a7ced09
tufusa <me@tufusa.net>
2026-05-25 16:05:09 +0900

    chore: テストを追加

---
+++ test/array.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/Void
+# rubocop:disable Style/EmptyLiteral
+# rubocop:disable Style/RedundantArrayConstructor
+
+arr = [1, 2, 3]
+
+Array.new
+Array.new 10
+Array.new 10, ''
+
+Array[]
+Array[1, 2, 3]
+
+arr + []
+arr + [2]
+arr + ['2']
+
+arr[0]
+arr[0..1]
+arr[0..]
+arr[...2]
+arr[nil...]
+arr[0, 2]
+
+arr.at 0
+arr.at 0..1
+arr.at 0..nil
+arr.at nil...2
+arr.at nil...nil
+arr.at 0, 2
+
+arr[0] = 1
+arr[0, 2] = 1
+arr[0, 2] = [2, 3, 4]
+
+arr.clear
+
+arr.difference []
+arr.difference [], [1], ['a']
+
+arr - []
+arr.-([], [1], ['a'])
+
+arr.delete_at 0
+
+arr.empty?
+
+arr.size
+
+arr.length
+
+arr.count
+
+arr.include? 0
+
+arr & []
+arr & [1, 2]
+arr & [1, 'a', nil]
+
+arr | []
+arr | [4]
+arr | [4, 'a', nil]
+
+arr.first
+
+arr.last
+
+arr.push 1
+
+arr << 1
+
+arr.pop
+arr.pop 2
+
+arr.shift
+arr.shift 2
+
+arr.unshift 1
+
+arr.dup
+
+arr.min
+
+arr.max
+
+arr.minmax
+
+arr.uniq
+
+arr.uniq!
+
+arr.reverse
+
+arr.reverse!
+
+arr.inspect
+Array.inspect
+
+arr.to_s
+Array.to_s
+
+arr.join
+arr.join ','
+arr.join 123
+
+# rubocop:enable all
