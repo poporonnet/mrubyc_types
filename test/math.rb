f9955af2614079aba780d8fbe35094742ccbb84c
tufusa <me@tufusa.net>
2026-05-25 12:19:11 +0900

    feat: `Math` を定義 (#45)
    * feat: `Math` を定義
    
    * chore: テストを追加

---
+++ test/math.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# rubocop:disable Lint/Void
+
+Math::E
+
+Math::PI
+
+Math.acos 0
+Math.acos 0.0
+
+Math.acosh 1
+Math.acosh 1.0
+
+Math.asin 0
+Math.asin 0.0
+
+Math.asinh 0
+Math.asinh 0.0
+
+Math.atan 0
+Math.atan 0.0
+
+Math.atan2 0, 1
+Math.atan2 0.0, 1.0
+
+Math.atanh 0
+Math.atanh 0.0
+
+Math.cbrt 0
+Math.cbrt 0.0
+
+Math.cos 0
+Math.cos 0.0
+
+Math.cosh 0
+Math.cosh 0.0
+
+Math.erf 0
+Math.erf 0.0
+
+Math.erfc 0
+Math.erfc 0.0
+
+Math.exp 0
+Math.exp 0.0
+
+Math.hypot 1, 1
+Math.hypot 1.0, 1.0
+
+Math.ldexp 0, 0
+Math.ldexp 0.0, 0.0
+
+Math.log 1
+Math.log 1.0
+
+Math.log10 1
+Math.log10 1.0
+
+Math.log2 1
+Math.log2 1.0
+
+Math.sin 0
+Math.sin 0.0
+
+Math.sinh 0
+Math.sinh 0.0
+
+Math.sqrt 0
+Math.sqrt 0.0
+
+Math.tan 0
+Math.tan 0.0
+
+Math.tanh 0
+Math.tanh 0.0
+
+# rubocop:enable all
